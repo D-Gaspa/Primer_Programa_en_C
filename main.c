98cf63329a8c711dabf830d694df7e5103ac0d34
D-Gaspa <diego.gaspariseo@udlap.mx>
2023-01-14 15:46:22 -0600

    First commit

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+// Function to get the area of a rectangle
+int getRectangleArea(int length, int width) {
+    int area = length * width;
+    return area;
+}//end getRectangleArea
+
+// Basic program to calculate the area of a rectangle
+int main() {
+    int length, width, area;
+    // Get the length of the rectangle
+    printf("Enter the length of the rectangle:\n");
+    scanf("%d", &length);
+
+    // Basic input validation to make sure the length is greater than 0
+    if (length <= 0) {
+        printf("The entered value is invalid, try again!");
+        return 0;
+    }//end if
+
+    // Get the width of the rectangle
+    printf("Enter the width of the rectangle:\n");
+    scanf("%d", &width);
+
+    // Basic input validation to make sure the width is greater than 0
+    if (width <= 0) {
+        printf("The entered value is invalid, try again!");
+        return 0;
+    }//end if
+
+    // Get the area of the rectangle
+    area = getRectangleArea(length, width);
+
+    // Print the area of the rectangle
+    printf("The area of the rectangle is = %d\n", area);
+    return 0;
+}//end main
